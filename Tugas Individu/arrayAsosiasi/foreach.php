900c31116d318d56e94a956bca3c7e7e1efa2901
NaufalNur25 <71967941+NaufalNur25@users.noreply.github.com>
2021-11-17 12:32:31 +0700

    Add files via upload

---
+++ Tugas Individu/arrayAsosiasi/foreach.php
@@ -0,0 +1,15 @@
+<?php 
+
+	$nama = array(
+		"salma" => "202121",
+		"silmi" => "202221",
+		"dhita" => "202222",
+		"sopia" => "202331",
+		"mila" => "202333");
+
+	foreach($nama as $name => $NIS){
+	echo "HAi $name NIS kamu adalah $NIS <br>"	;
+	}
+	
+
+ ?>
